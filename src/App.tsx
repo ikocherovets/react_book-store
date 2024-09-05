56945f413787c37f2c575f8768dced6a781e16ba
Illia Kocherovets <illiakocherovets@gmail.com>
2024-09-05 20:46:44 +0200

    Install Bulma and import styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BookApp } from './components/BookApp';
 
-
-
 export const App: React.FC = () => {
   return (
     <div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx'
 import './index.css'
+import 'bulma/css/bulma.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
